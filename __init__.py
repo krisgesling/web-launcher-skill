d97aef2019f69f29b594a21a31c52d860db6c27d
Kris Gesling <kris.gesling@mycroft.ai>
2020-07-26 11:05:08 +0930

    refactor settings callback; add doc strings

--- __init__.py
+++ __init__.py
@@ -12,29 +12,40 @@ class WebLauncher(MycroftSkill):
     def initialize(self):
         self.settings_change_callback = self.on_settings_changed
         self.on_settings_changed()
+
+    def on_settings_changed(self):
+        """Load all custom site settings and register names as vocab.
+        
+        If expanding number of custom url settings available remember to 
+        increment range stop.
+        """
+        for num in range(1, 6):
+            name = self.settings.get('name_' + str(num))
+            url = self.settings.get('url_' + str(num))
+            if name and url:
+                self.sites[name] = url
         self.register_sites_as_vocab()
 
     def register_sites_as_vocab(self):
+        """Registers site names as vocab for Adapt intents."""
         for site_name in self.sites:
             self.register_vocabulary(site_name.lower(), 'SiteName')
 
-    def on_settings_changed(self):
-        options = [('name_one', 'url_one'), 
-                   ('name_two', 'url_two'), 
-                   ('name_three', 'url_three')]
-        for o in options:
-            name = self.settings.get(o[0])
-            url = self.settings.get(o[1])
-            if name and url:
-                self.sites[name] = url
-
-    @intent_handler(IntentBuilder('LaunchSite').require('Launch').require('SiteName'))
+    @intent_handler(IntentBuilder('LaunchSite').require('Launch')
+                                               .require('SiteName'))
     def handle_launch_site(self, message):
+        """Launch known site using xdg-open url.
+        
+        Primary intent handler for this Skill. Uses Adapt so that only
+        utterances that include a known site are triggered. Prevents conflicts
+        with other intents that also use vocabularly like "launch" or "open".
+        """
         requested_site = message.data.get('SiteName')
         if requested_site in self.sites:
             args = ['xdg-open', self.sites[requested_site]]
             current_process = subprocess.Popen(args)
         else:
+            # This should never actually be triggered
             self.speak_dialog('not.found')
 
 
