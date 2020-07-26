987b59a1a2ae1e603f26c7f25fc1cb4f4007a2df
Kris Gesling <kris.gesling@mycroft.ai>
2020-07-26 10:36:36 +0930

    switch to adapt - only match known sites

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 import subprocess
 
-from mycroft import MycroftSkill, intent_file_handler
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_handler
 
 
 class WebLauncher(MycroftSkill):
@@ -11,6 +12,11 @@ class WebLauncher(MycroftSkill):
     def initialize(self):
         self.settings_change_callback = self.on_settings_changed
         self.on_settings_changed()
+        self.register_sites_as_vocab()
+
+    def register_sites_as_vocab(self):
+        for site_name in self.sites:
+            self.register_vocabulary(site_name.lower(), 'SiteName')
 
     def on_settings_changed(self):
         options = [('name_one', 'url_one'), 
@@ -22,9 +28,9 @@ class WebLauncher(MycroftSkill):
             if name and url:
                 self.sites[name] = url
 
-    @intent_file_handler('launcher.web.intent')
-    def handle_launcher_web(self, message):
-        requested_site = message.data.get('site')
+    @intent_handler(IntentBuilder('LaunchSite').require('Launch').require('SiteName'))
+    def handle_launch_site(self, message):
+        requested_site = message.data.get('SiteName')
         if requested_site in self.sites:
             args = ['xdg-open', self.sites[requested_site]]
             current_process = subprocess.Popen(args)
