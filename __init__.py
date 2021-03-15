dcc7cea6bb2d3bd5d9d94bf39140948341249918
Kris Gesling <kris.gesling@mycroft.ai>
2021-03-15 15:30:52 +0930

    auto-format

--- __init__.py
+++ __init__.py
@@ -14,17 +14,17 @@ class WebLauncher(MycroftSkill):
         self.settings_change_callback = self.on_settings_changed
         self.on_settings_changed()
         if self.gui.connected:
-            self.register_intent('CloseSite.intent', self.handle_close_site)
+            self.register_intent("CloseSite.intent", self.handle_close_site)
 
     def on_settings_changed(self):
         """Load all custom site settings and register names as vocab.
-        
-        If expanding number of custom url settings available remember to 
+
+        If expanding number of custom url settings available remember to
         increment range stop.
         """
         for num in range(1, 6):
-            name = self.settings.get('name_' + str(num))
-            url = self.settings.get('url_' + str(num))
+            name = self.settings.get("name_" + str(num))
+            url = self.settings.get("url_" + str(num))
             if name and url:
                 self.sites[name] = url
         self.register_sites_as_vocab()
@@ -32,31 +32,33 @@ class WebLauncher(MycroftSkill):
     def register_sites_as_vocab(self):
         """Registers site names as vocab for Adapt intents."""
         for site_name in self.sites:
-            self.register_vocabulary(site_name.lower(), 'SiteName')
+            self.register_vocabulary(site_name.lower(), "SiteName")
 
-    @intent_handler(IntentBuilder('LaunchSite').require('SiteName')
-                                               .require('Launch'))
+    @intent_handler(IntentBuilder("LaunchSite").require("SiteName").require("Launch"))
     def handle_launch_site(self, message):
         """Launch known site using xdg-open url.
-        
+
         Primary intent handler for this Skill. Uses Adapt so that only
         utterances that include a known site are triggered. Prevents conflicts
         with other intents that also use vocabularly like "launch" or "open".
         """
-        requested_site = message.data.get('SiteName')
+        requested_site = message.data.get("SiteName")
         if requested_site in self.sites:
             site_url = self.sites[requested_site]
             if self.gui.connected:
                 self.gui.show_url(site_url, override_idle=True)
             else:
-                args = ['xdg-open', site_url]
+                args = ["xdg-open", site_url]
                 current_process = subprocess.Popen(args)
-            self.bus.emit(Message('skill.weblauncher.opening', {
-                                      'site_name': requested_site,
-                                      'site_url': site_url }))
+            self.bus.emit(
+                Message(
+                    "skill.weblauncher.opening",
+                    {"site_name": requested_site, "site_url": site_url},
+                )
+            )
         else:
             # This should never actually be triggered
-            self.speak_dialog('not.found')
+            self.speak_dialog("not.found")
 
     def handle_close_site(self, message):
         """Close the website if opened on a Mycroft based GUI."""
@@ -66,4 +68,3 @@ class WebLauncher(MycroftSkill):
 
 def create_skill():
     return WebLauncher()
-
